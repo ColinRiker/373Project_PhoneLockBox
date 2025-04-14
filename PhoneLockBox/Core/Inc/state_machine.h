c51eb21669fbc45df058a4fa3cb3d6f418ddc3c3
YOUR NAME HERE <blongo@umich.edu>
2025-04-14 11:19:37 -0400

    updated state machine logic on stateLogic branch

--- PhoneLockBox/Core/Inc/state_machine.h
+++ PhoneLockBox/Core/Inc/state_machine.h
@@ -1,5 +1,26 @@
 #include "shared.h"
 
+typedef enum {
+	SFLAG_NULL,
+	SFLAG_ACC_BOX_MOVED,
+	SFLAG_ADC_INTERRUPT,
+	SFLAG_ADC_MATCH,
+	SFLAG_ROTENC_INTERRUPT, //if rotenc button is pressed
+	SFLAG_ROTENC_ROTATED, //if rotenc dial is rotated
+	SFLAG_TIMER_COMPLETE, //this will be set for whatever time we need dependent on the state
+	SFLAG_NFC_PHONE_PRESENT,
+
+	//added flags
+	SFLAG_BOX_OPEN,
+	SFLAG_BOX_CLOSED,
+	SFLAG_AUDIO_VOL_HIGH, //is audio volume high
+	SFLAG_AUDIO_MATCH //is there an audio match
+} SFlag;
+
+
+
+
+
 BoxMode runStateMachine(void);
 void stateMachineInit(void);
 //THESE NEED TO BE MOVED EVENTUALLY
--- PhoneLockBox/Core/Src/state_machine.c
+++ PhoneLockBox/Core/Src/state_machine.c
@@ -9,6 +9,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_FLAGS 8
+
+SFlag flags[MAX_FLAGS];
 
 extern BoxState state;
 extern BoxState next_state;
@@ -42,7 +45,7 @@ BoxMode runStateMachine(void) {
 
 	case UNLOCKED_EMPTY_ASLEEP:
 		// if the box moves, wake up
-		if (accResolve()==UNLOCKED_ASLEEP_TO_AWAKE) {
+		if (SFLAG_ACC_BOX_MOVED || SFLAG_ROTENC_INTERRUPT || SFLAG_ROTENC_ROTATED) {
 		      next = UNLOCKED_ASLEEP_TO_AWAKE;
 		}
 		break;
@@ -51,83 +54,79 @@ BoxMode runStateMachine(void) {
 		// show hello msg / animate screen on
 		// we only move to one other state form this state
 		//note that timer resolve is declared but has no current implementation
-		if(timerResolve()==UNLOCKED_EMPTY_AWAKE) {
+		if(SFLAG_TIMER_COMPLETE) {
 			next = UNLOCKED_EMPTY_AWAKE;
-
 		}
 		break;
 
 	case UNLOCKED_EMPTY_AWAKE:
 		//THIS TAKES PRIORITY OVER GOING BACK TO SLEEP
-		if (NFCResolve()==UNLOCKED_FULL_AWAKE_FUNC_A) {
+		if (SFLAG_NFC_PHONE_PRESENT) {
 			next = UNLOCKED_FULL_AWAKE_FUNC_A;
 		}
 		//if we cannot move to full awake, we move back to sleep
-		else if((rotencResolve()==UNLOCKED_EMPTY_ASLEEP) && (timerResolve()==UNLOCKED_EMPTY_ASLEEP)) {
+		else if(SFLAG_TIMER_COMPLETE && SFLAG_ROTENC_INTERRUPT) {
 			next = UNLOCKED_EMPTY_ASLEEP;
 		}
-		// if user does nothing for a while, go back to sleep
-		// TODO: add timer / timeout
+
 		break;
 
 	case UNLOCKED_FULL_AWAKE_FUNC_A:
-		//who wrote that lol not sure what to do with this line
-		 if (/* isButtonHeldLong() */ 0) {
-			 next = UNLOCKED_TO_LOCKED_AWAKE;
 		 // PRIORITIZE MOVING TO AWAKE FUNC B
-		 if (rotencResolve()==UNLOCKED_FULL_AWAKE_FUNC_B) {
+		 if (SFLAG_ROTENC_ROTATED) {
 			 next=UNLOCKED_FULL_AWAKE_FUNC_B;
-		 } //if we can't move to taht, we move back to sleep
-		 else if(timerResolve()==UNLOCKED_FULL_ASLEEP) {
+		 } //if we can't move to that, we move back to sleep
+		 else if(SFLAG_TIMER_COMPLETE) {
 			 next=UNLOCKED_FULL_ASLEEP;
 		 }
 		break;
 	case UNLOCKED_FULL_AWAKE_FUNC_B:
 		// PRIORITIZE MOVING TO UNLOCKED_TO_LOCKED_AWAKE
-		if (rotencResolve()==UNLOCKED_TO_LOCKED_AWAKE && accResolve()==UNLOCKED_TO_LOCKED_AWAKE) {
+		if (SFLAG_ROTENC_INTERRUPT && SFLAG_BOX_CLOSED) {
 			next=UNLOCKED_TO_LOCKED_AWAKE;
 		} //if we can't move to UNLOCKED_TO_LOCKED_AWAKE, we move to func A
-		else if(rotencResolve()==UNLOCKED_FULL_AWAKE_FUNC_A) {
-			next=UNLOCKED_FULL_ASLEEP;
+		else if(SFLAG_ROTENC_ROTATED) {
+			next=UNLOCKED_FULL_AWAKE_FUNC_A;
 		}  //next, move back to unlocked and empty but awake
-		else if(NFCResolve()==UNLOCKED_EMPTY_AWAKE) {
+		else if(!SFLAG_NFC_PHONE_PRESENT) {
 			next=UNLOCKED_EMPTY_AWAKE;
 		} //finally, we last move back to sleep
-		else if(timerResolve()==UNLOCKED_FULL_ASLEEP) {
+		else if(SFLAG_TIMER_COMPLETE) {
 			next=UNLOCKED_FULL_ASLEEP;
 		}
 		break;
 	case UNLOCKED_FULL_ASLEEP:
 		 // PRIORITIZE MOVING BACK TO EMPTY IF PHONE IS EVER TAKEN OUT
-		 if (NFCResolve()==UNLOCKED_EMPTY_AWAKE) {
+		 if (!SFLAG_NFC_PHONE_PRESENT) {
 			 next=UNLOCKED_EMPTY_AWAKE;
 		 }// if not, wake back up if possible
-		 else if(rotencResolve()==UNLOCKED_FULL_AWAKE_FUNC_A && accResolve()==UNLOCKED_FULL_AWAKE_FUNC_A) {
+		 else if(SFLAG_ACC_BOX_MOVED || SFLAG_ROTENC_ROTATED || SFLAG_ROTENC_INTERRUPT) {
 			 next=UNLOCKED_FULL_AWAKE_FUNC_A;
 		 }
 		break;
 	case UNLOCKED_TO_LOCKED_AWAKE:
-		// if the button is pressed but there is NO phone at any point
+		// if the button is pressed OR there is NO phone at any point OR box is opened
 		// prioritize this
-		if(accResolve()==UNLOCKED_FULL_AWAKE_FUNC_B && rotencResolve()==UNLOCKED_FULL_AWAKE_FUNC_B) {
+		if((!SFLAG_NFC_PHONE_PRESENT) || SFLAG_BOX_OPEN || SFLAG_ROTENC_INTERRUPT) {
 			next = UNLOCKED_FULL_AWAKE_FUNC_B;
-		} // if the button is pressed, thre IS a phone and it has been 5 seconds
-		else if(timerResolve()==LOCKED_FULL_AWAKE && accResolve()==LOCKED_FULL_AWAKE && rotencResolve()==LOCKED_FULL_AWAKE) {
+		} // if it has been 5 seconds in this state
+		else if(SFLAG_TIMER_COMPLETE) {
 			next = LOCKED_FULL_AWAKE;
 		}
 		break;
 
 	case LOCKED_FULL_AWAKE:
 		//if at any point phone not in box, enter emergency state
-		if(accResolve()==EMERGENCY_OPEN) {
+		if(SFLAG_BOX_OPEN) {
 			next = EMERGENCY_OPEN;
 		}
-		// this is the main countdown state
-		if (state.time > 0) {
-			state.time--;
-		} else {
-			// unlock and go back to unlocked state
-			next = UNLOCKED_EMPTY_AWAKE;
+		//sleep if timer hits 1 min
+		else if(SFLAG_TIMER_COMPLETE) {
+			next = LOCKED_FULL_ASLEEP;
+		}
+		//start monitoring audio
+		else if(SFLAG_AUDIO_VOL_HIGH) {
+			next = LOCKED_MONITOR_AWAKE;
 		}
 
 		// optional: monitor for emergency unlock here
@@ -135,72 +134,77 @@ BoxMode runStateMachine(void) {
 
 	case LOCKED_FULL_NOTIFICATION_FUNC_A:
 		//if at any point phone not in box, enter emergency state
-		if(accResolve()==EMERGENCY_OPEN) {
+		if(SFLAG_BOX_OPEN) {
 			next = EMERGENCY_OPEN;
 		}
 		//if we hold button in this state, unlock
-		else if(rotarResolve()==UNLOCKED_FULL_AWAKE_FUNC_B) {
+		else if(SFLAG_ROTENC_INTERRUPT) {
 			next = UNLOCKED_FULL_AWAKE_FUNC_B;
 		} //if we MOVE dial in any way, then we are changing selection
-		else if(rotarResolve()=LOCKED_FULL_NOTIFICATION_FUNC_B) {
+		else if(SFLAG_ROTENC_ROTATED) {
 			next = LOCKED_FULL_NOTIFICATION_FUNC_B;
 		} // if it's been a minute, assume they are choosing to ignore and go bck to locked (but awake)
-		else if(timerResolve()==LOCKED_FULL_AWAKE) {
+		else if(SFLAG_TIMER_COMPLETE) {
 			next=LOCKED_FULL_AWAKE;
 		}
 		break;
 	case LOCKED_FULL_NOTIFICATION_FUNC_B:
 		//if at any point phone not in box, enter emergency state
-		if(accResolve()==EMERGENCY_OPEN) {
+		if(SFLAG_BOX_OPEN) {
 			next = EMERGENCY_OPEN;
 		}
-		else if(rotarResolve()=LOCKED_FULL_NOTIFICATION_FUNC_A) {
+		//
+		else if(SFLAG_ROTENC_ROTATED) {
 			next = LOCKED_FULL_NOTIFICATION_FUNC_A;
 		} // if it's been a minute, assume they are choosing to ignore and go bck to locked (but awake)
-		else if(timerResolve()==LOCKED_FULL_AWAKE) {
+		else if(SFLAG_TIMER_COMPLETE) {
 			next=LOCKED_FULL_AWAKE;
 		}
-
 		break;
 	case LOCKED_FULL_ASLEEP:
 		//if at any point phone not in box, enter emergency state
-		if(accResolve()==EMERGENCY_OPEN) {
+		if(SFLAG_BOX_OPEN) {
 			next = EMERGENCY_OPEN;
 		}
-		else if() {
-
+		//if volume is high, we do not wake up monitor but we do listen to sound
+		else if(SFLAG_AUDIO_VOL_HIGH) {
+			next = LOCKED_MONITOR_ASLEEP;
 		}
-		else if() {
-
+		//if button pressed OR dial changed OR box moved, wake up
+		else if(SFLAG_ROTENC_INTERRUPT || SFLAG_ROTENC_ROTATED || SFLAG_ACC_BOX_MOVED) {
+			next = LOCKED_FULL_AWAKE;
 		}
 		break;
 	case LOCKED_MONITOR_AWAKE:
 		//if at any point phone not in box, enter emergency state
-		if(accResolve()==EMERGENCY_OPEN) {
+		if(SFLAG_BOX_OPEN) {
 			next = EMERGENCY_OPEN;
 		}
-		else if() {
-
+		//if there is a match, we move to prompt user if they want to unlock
+		else if(SFLAG_AUDIO_MATCH) {
+			next = LOCKED_FULL_NOTIFICATION_FUNC_B;
+		} else { //otherwise (if no match) we move back to locked state
+			next = LOCKED_FULL_AWAKE;
 		}
-		else if() {
 
-		}
 		break;
 	case LOCKED_MONITOR_ASLEEP:
 		//if at any point phone not in box, enter emergency state
-		if(accResolve()==EMERGENCY_OPEN) {
+		if(SFLAG_BOX_OPEN) {
 			next = EMERGENCY_OPEN;
 		}
-		else if() {
-
+		//if there is an audio match, prompt user
+		else if(SFLAG_AUDIO_MATCH) {
+			next = LOCKED_FULL_NOTIFCATION_B;
 		}
-		else if() {
-
+		//if there was no audio match, go back to sleep
+		else  {
+			next = LOCKED_FULL_ASLEEP;
 		}
 		break;
 	case EMERGENCY_OPEN:
 		//if we were in this for 5 seconds, move to UNLOCKED_FULL_AWAKE_FUNC_B
-		if(timerResolve()==UNLOCKED_FULL_AWAKE_FUNC_B) {
+		if(SFLAG_TIMER_COMPLETE) {
 			next=UNLOCKED_FULL_AWAKE_FUNC_B
 		}
 		break;
@@ -219,5 +223,3 @@ BoxMode runStateMachine(void) {
 
 	}
 
-	return state.mode;
-}
