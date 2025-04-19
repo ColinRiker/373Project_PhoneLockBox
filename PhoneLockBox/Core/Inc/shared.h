62b825df675c6e59b648ebc0b4f1ef26482ccba9
YOUR NAME HERE <blongo@umich.edu>
2025-04-19 16:40:32 -0400

    Display updates: screen driver and related files

---
+++ PhoneLockBox/Core/Inc/shared.h
@@ -0,0 +1,104 @@
+/*
+ * shared.h
+ *
+ *  Created on: Mar 28, 2025
+ *      Author: colinriker
+ */
+
+#ifndef INC_SHARED_H_
+#define INC_SHARED_H_
+
+
+#define I2C_TIMEOUT 1000
+#define DEBUG_OUT //Comment out to not compile debug functions and statement
+
+#ifdef DEBUG_OUT
+#define DEBUG_EVENT_CONTROLLER
+#define DEBUG_NFC
+//#define DEBUG_DISPLAY
+//#define DEBUG_AUDIO
+#define DEBUG_ROTARY_ENCODER
+#define DEBUG_STATE_CONTROLLER
+//#define DEBUG_ACC_MAG
+#endif /*END DEBUG DEFINES*/
+
+#include <string.h>
+#include <stdint.h>
+
+enum {
+	UNLOCKED_EMPTY_ASLEEP,
+	UNLOCKED_ASLEEP_TO_AWAKE,
+	UNLOCKED_EMPTY_AWAKE,
+	UNLOCKED_FULL_AWAKE_FUNC_A,
+	UNLOCKED_FULL_AWAKE_FUNC_B,
+	UNLOCKED_FULL_ASLEEP,
+	UNLOCKED_TO_LOCKED_AWAKE,
+	LOCKED_FULL_AWAKE,
+	LOCKED_FULL_ASLEEP,
+	LOCKED_MONITOR_AWAKE,
+	LOCKED_MONITOR_ASLEEP,
+	LOCKED_FULL_NOTIFICATION_FUNC_A,
+	LOCKED_FULL_NOTIFICATION_FUNC_B,
+	EMERGENCY_OPEN
+} typedef BoxState;
+
+
+typedef struct {
+	BoxState mode;
+} BoxState_old;
+
+typedef struct {
+	int16_t x_componenet;
+	int16_t y_componenet;
+	int16_t z_componenet;
+} Vector3D;
+
+/* Global Variables */
+extern BoxState previous;
+extern BoxState state;
+extern BoxState next_state;
+/* End Global Variables */
+
+static inline uint16_t VectorDelta(Vector3D *vec, Vector3D *prev_vec){
+	return (vec->x_componenet - prev_vec->x_componenet) +
+			(vec->y_componenet - prev_vec->y_componenet) +
+			(vec->z_componenet - prev_vec->z_componenet);
+}
+
+
+static inline const char* stateToStr(BoxState boxstate) {
+
+	switch (boxstate) {
+	case UNLOCKED_EMPTY_ASLEEP: return "Unlocked Empty Asleep";
+
+	case UNLOCKED_ASLEEP_TO_AWAKE: return "Unlocked Asleep to Awake";
+
+	case UNLOCKED_EMPTY_AWAKE: return "Unlocked Empty Awake";
+
+	case UNLOCKED_FULL_AWAKE_FUNC_A: return "Unlocked Full Awake Function A";
+
+	case UNLOCKED_FULL_AWAKE_FUNC_B: return "Unlocked Full Awake Function B";
+
+	case UNLOCKED_FULL_ASLEEP: return "Unlocked Full Asleep";
+
+	case UNLOCKED_TO_LOCKED_AWAKE: return "Unlocked to Locked Awake";
+
+	case LOCKED_FULL_AWAKE: return "Locked Full Awake";
+
+	case LOCKED_FULL_ASLEEP: return "Locked Full Asleep";
+
+	case LOCKED_MONITOR_AWAKE: return "Locked Monitor Awake";
+
+	case LOCKED_MONITOR_ASLEEP: return "Locked Monitor Asleep";
+
+	case LOCKED_FULL_NOTIFICATION_FUNC_A: return "Locked Full Notification Function A";
+
+	case LOCKED_FULL_NOTIFICATION_FUNC_B: return "Locked Full Notification Function B";
+
+	case EMERGENCY_OPEN: return "Emergency Open";
+
+	default: return "[ERROR] Undefined State";
+	}
+}
+
+#endif /* INC_SHARED_H_ */
---
+++ PhoneLockBox/Core/Src/state_machine.c
@@ -0,0 +1,434 @@
+/*
+ * State Machine
+ * Handles the various transitions and functions of the box
+ *
+ * */
+
+#include "state_machine.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Screen_Driver.h"
+
+#include "accelerometer.h"
+#include "audio.h"
+#include "event_controller.h"
+#include "nfc.h"
+#include "rotary_encoder.h"
+#include "shared.h"
+#include "main.h"
+
+#define MINUTE 60000
+
+BoxState state;
+BoxState previous;
+
+SFlag flags[MAX_FLAGS];
+bool master_timer_done; //Master time is the lock timer
+
+bool hasFlag(SFlag flag) {
+	for (uint8_t i = 0; i < MAX_FLAGS; ++i) {
+		if (flags[i] == flag) return true;
+	}
+	return false;
+}
+
+bool stateInsertFlag(SFlag flag) {
+#ifdef DEBUG_STATE_CONTROLLER
+	printf("[INFO] Inserting State flag: %s\n\r", SFlagToStr(flag));
+#endif
+
+	uint8_t empty_idx = MAX_FLAGS;
+	for(uint8_t i = 0; i < MAX_FLAGS; ++i) {
+		if (flags[i] == flag) {
+			return false;
+		} else if (flags[i] == SFLAG_NULL && i < empty_idx) {
+			empty_idx = i;
+		}
+	}
+
+	if (empty_idx < MAX_FLAGS) {
+		flags[empty_idx] = flag;
+		return true;
+	}
+
+	return false;
+
+}
+
+void clearFlags(void) {
+	for (uint8_t i = 0; i < MAX_FLAGS; ++i) {
+		flags[i] = SFLAG_NULL;
+	}
+}
+
+void inturruptControl(void) {
+
+	switch(state) {
+	//all states that need to have button DISABLED
+	case UNLOCKED_ASLEEP_TO_AWAKE:
+	case UNLOCKED_FULL_AWAKE_FUNC_A:
+	case LOCKED_FULL_AWAKE:
+	case LOCKED_MONITOR_AWAKE:
+	case LOCKED_MONITOR_ASLEEP:
+	case EMERGENCY_OPEN:
+		//func to DISABLE button interrupt
+		HAL_NVIC_DisableIRQ(EXTI15_10_IRQn);  // button
+		break;
+		//all states that need to have button ENABLED
+	case UNLOCKED_EMPTY_ASLEEP:
+	case UNLOCKED_EMPTY_AWAKE:
+	case UNLOCKED_FULL_ASLEEP:
+	case UNLOCKED_FULL_AWAKE_FUNC_B:
+	case UNLOCKED_TO_LOCKED_AWAKE:
+	case LOCKED_FULL_NOTIFICATION_FUNC_A:
+	case LOCKED_FULL_NOTIFICATION_FUNC_B:
+	case LOCKED_FULL_ASLEEP:
+		//func to ENABLE button interrupt
+		HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+		break;
+	}
+
+	switch(state) {
+	//all states that need to have audio DISABLED
+	case UNLOCKED_EMPTY_ASLEEP:
+	case UNLOCKED_ASLEEP_TO_AWAKE:
+	case UNLOCKED_EMPTY_AWAKE:
+	case UNLOCKED_FULL_AWAKE_FUNC_A:
+	case UNLOCKED_FULL_AWAKE_FUNC_B:
+	case UNLOCKED_FULL_ASLEEP:
+	case UNLOCKED_TO_LOCKED_AWAKE:
+	case LOCKED_FULL_NOTIFICATION_FUNC_A:
+	case LOCKED_FULL_NOTIFICATION_FUNC_B:
+	case EMERGENCY_OPEN:
+		//func to DISABLE audio interrupt
+		HAL_NVIC_DisableIRQ(EXTI0_IRQn);      // audio
+		break;
+		//all states that need to have audio ENABLED
+	case LOCKED_MONITOR_AWAKE:
+	case LOCKED_FULL_ASLEEP:
+	case LOCKED_FULL_AWAKE:
+	case LOCKED_MONITOR_ASLEEP:
+		//func to ENABLE audio interrupt
+		HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+		break;
+
+	}
+}
+
+
+void stateMachineInit(void) {
+	previous = UNLOCKED_EMPTY_ASLEEP;
+	state = UNLOCKED_EMPTY_ASLEEP;
+}
+
+void runStateMachine(void) {
+	BoxState next = state;
+
+	switch (state) {
+
+	case UNLOCKED_EMPTY_ASLEEP:
+		// if the box moves, wake up
+		if (hasFlag(SFLAG_ACC_BOX_MOVED) || hasFlag(SFLAG_ROTENC_INTERRUPT) || hasFlag(SFLAG_ROTENC_ROTATED)) {
+			next = UNLOCKED_ASLEEP_TO_AWAKE;
+		}
+		break;
+
+	case UNLOCKED_ASLEEP_TO_AWAKE:
+		// show hello msg / animate screen on
+		// we only move to one other state form this state
+		//note that timer resolve is declared but has no current implementation
+		if(hasFlag(SFLAG_TIMER_COMPLETE)) {
+			next = UNLOCKED_EMPTY_AWAKE;
+		}
+		break;
+
+	case UNLOCKED_EMPTY_AWAKE:
+		//THIS TAKES PRIORITY OVER GOING BACK TO SLEEP
+		if (hasFlag(SFLAG_NFC_PHONE_PRESENT)) {
+			next = UNLOCKED_FULL_AWAKE_FUNC_A;
+		}
+		//if we cannot move to full awake, we move back to sleep
+		else if(hasFlag(SFLAG_TIMER_COMPLETE) || hasFlag(SFLAG_ROTENC_INTERRUPT)) {
+			next = UNLOCKED_EMPTY_ASLEEP;
+		}
+
+		break;
+
+	case UNLOCKED_FULL_AWAKE_FUNC_A:
+		//if we take out phone at ANY point, we gotta go back to UNLCOKED_EMPTY_AWAKE
+		if(hasFlag(SFLAG_NFC_PHONE_NOT_PRESENT)) {
+			next=UNLOCKED_EMPTY_AWAKE;
+		}
+		// PRIORITIZE MOVING TO AWAKE FUNC B
+		else if (hasFlag(SFLAG_ROTENC_ROTATED)) {
+			next=UNLOCKED_FULL_AWAKE_FUNC_B;
+		} //if we can't move to that, we move back to sleep
+		else if(hasFlag(SFLAG_TIMER_COMPLETE)) {
+			next=UNLOCKED_FULL_ASLEEP;
+		}
+		break;
+	case UNLOCKED_FULL_AWAKE_FUNC_B:
+		// PRIORITIZE MOVING TO UNLOCKED_TO_LOCKED_AWAKE
+		if (hasFlag(SFLAG_ROTENC_INTERRUPT) && hasFlag(SFLAG_BOX_CLOSED)) {
+			next=UNLOCKED_TO_LOCKED_AWAKE;
+		} //if we can't move to UNLOCKED_TO_LOCKED_AWAKE, we move to func A
+		else if(hasFlag(SFLAG_ROTENC_ROTATED)) {
+			next=UNLOCKED_FULL_AWAKE_FUNC_A;
+		}  //next, move back to unlocked and empty but awake
+		else if(hasFlag(SFLAG_NFC_PHONE_NOT_PRESENT)) {
+			next=UNLOCKED_EMPTY_AWAKE;
+		} //finally, we last move back to sleep
+		else if(hasFlag(SFLAG_TIMER_COMPLETE)) {
+			next=UNLOCKED_FULL_ASLEEP;
+		}
+		break;
+	case UNLOCKED_FULL_ASLEEP:
+		// PRIORITIZE MOVING BACK TO EMPTY IF PHONE IS EVER TAKEN OUT
+		if (hasFlag(SFLAG_NFC_PHONE_NOT_PRESENT)) {
+			next=UNLOCKED_EMPTY_AWAKE;
+		}// if not, wake back up if possible
+		else if(hasFlag(SFLAG_ACC_BOX_MOVED) || hasFlag(SFLAG_ROTENC_ROTATED) || hasFlag(SFLAG_ROTENC_INTERRUPT)) {
+			next=UNLOCKED_FULL_AWAKE_FUNC_A;
+		}
+		break;
+	case UNLOCKED_TO_LOCKED_AWAKE:
+		// if the button is pressed OR there is NO phone at any point OR box is opened
+		// prioritize this
+		if((hasFlag(SFLAG_NFC_PHONE_NOT_PRESENT)) || hasFlag(SFLAG_BOX_OPEN) || hasFlag(SFLAG_ROTENC_INTERRUPT)) {
+			next = UNLOCKED_FULL_AWAKE_FUNC_B;
+		} // if it has been 5 seconds in this state
+		else if(hasFlag(SFLAG_TIMER_COMPLETE)) {
+			next = LOCKED_FULL_AWAKE;
+		}
+		break;
+
+	case LOCKED_FULL_AWAKE:
+		//if at any point phone not in box, enter emergency state
+		if(hasFlag(SFLAG_BOX_OPEN)) {
+			next = EMERGENCY_OPEN;
+		}
+		//sleep if timer hits 1 min
+		else if(hasFlag(SFLAG_TIMER_COMPLETE)) {
+			next = LOCKED_FULL_ASLEEP;
+		}
+		//start monitoring audio
+		else if(hasFlag(SFLAG_AUDIO_VOL_HIGH)) {
+			next = LOCKED_MONITOR_AWAKE;
+		}
+
+		// optional: monitor for emergency unlock here
+		break;
+
+	case LOCKED_FULL_NOTIFICATION_FUNC_A:
+		//if at any point phone not in box, enter emergency state
+		if(hasFlag(SFLAG_BOX_OPEN)) {
+			next = EMERGENCY_OPEN;
+		}
+		//if we hold button in this state, unlock
+		else if(hasFlag(SFLAG_ROTENC_INTERRUPT)) {
+			next = UNLOCKED_FULL_AWAKE_FUNC_B;
+		} //if we MOVE dial in any way, then we are changing selection
+		else if(hasFlag(SFLAG_ROTENC_ROTATED)) {
+			next = LOCKED_FULL_NOTIFICATION_FUNC_B;
+		} // if it's been a minute, assume they are choosing to ignore and go bck to locked (but awake)
+		else if(hasFlag(SFLAG_TIMER_COMPLETE)) {
+			next=LOCKED_FULL_AWAKE;
+		}
+		break;
+	case LOCKED_FULL_NOTIFICATION_FUNC_B:
+		//if at any point phone not in box, enter emergency state
+		if(hasFlag(SFLAG_BOX_OPEN)) {
+			next = EMERGENCY_OPEN;
+		}
+		//
+		else if(hasFlag(SFLAG_ROTENC_ROTATED)) {
+			next = LOCKED_FULL_NOTIFICATION_FUNC_A;
+		} // if it's been a minute, assume they are choosing to ignore and go bck to locked (but awake)
+		else if(hasFlag(SFLAG_TIMER_COMPLETE)) {
+			next=LOCKED_FULL_AWAKE;
+		}
+		break;
+	case LOCKED_FULL_ASLEEP:
+		//if at any point phone not in box, enter emergency state
+		if(hasFlag(SFLAG_BOX_OPEN)) {
+			next = EMERGENCY_OPEN;
+		}
+		//if volume is high, we do not wake up monitor but we do listen to sound
+		else if(hasFlag(SFLAG_AUDIO_VOL_HIGH)) {
+			next = LOCKED_MONITOR_ASLEEP;
+		}
+		//if button pressed OR dial changed OR box moved, wake up
+		else if(hasFlag(SFLAG_ROTENC_INTERRUPT) || hasFlag(SFLAG_ROTENC_ROTATED) || hasFlag(SFLAG_ACC_BOX_MOVED)) {
+			next = LOCKED_FULL_AWAKE;
+		}
+		break;
+	case LOCKED_MONITOR_AWAKE:
+		//if at any point phone not in box, enter emergency state
+		if(hasFlag(SFLAG_BOX_OPEN)) {
+			next = EMERGENCY_OPEN;
+		}
+		//if there is a match, we move to prompt user if they want to unlock
+		else if(hasFlag(SFLAG_AUDIO_MATCH)) {
+			next = LOCKED_FULL_NOTIFICATION_FUNC_B;
+		} else { //otherwise (if no match) we move back to locked state
+			next = LOCKED_FULL_AWAKE;
+		}
+
+		break;
+	case LOCKED_MONITOR_ASLEEP:
+		//if at any point phone not in box, enter emergency state
+		if(hasFlag(SFLAG_BOX_OPEN)) {
+			next = EMERGENCY_OPEN;
+		}
+		//if there is an audio match, prompt user
+		else if(hasFlag(SFLAG_AUDIO_MATCH)) {
+			next = LOCKED_FULL_NOTIFICATION_FUNC_B;
+		}
+		//if there was no audio match, go back to sleep
+		else  {
+			next = LOCKED_FULL_ASLEEP;
+		}
+		break;
+	case EMERGENCY_OPEN:
+		//if we were in this for 5 seconds, move to UNLOCKED_FULL_AWAKE_FUNC_B
+		if(hasFlag(SFLAG_TIMER_COMPLETE)) {
+			next=UNLOCKED_FULL_AWAKE_FUNC_B;
+		}
+		break;
+	default:
+#ifdef DEBUG_STATE_CONTROLLER
+		printf("[ERROR] Default case of state machine reached, system in bad state\n\r");
+#endif
+		break;
+	}
+
+	if(master_timer_done) { //if we trigger overall timer
+		next = UNLOCKED_FULL_AWAKE_FUNC_A;
+		//reset
+		master_timer_done=false;
+	}
+
+	// update state if changed
+	if (next != state) {
+		/* State Cleanup*/
+		eventClear();
+		clearFlags();
+
+#ifdef DEBUG_STATE_CONTROLLER
+		printf("\n[Info] --- State transition: %s → %s ---\n\r", stateToStr(state), stateToStr(next));
+#endif
+		previous = state;
+		state = next;
+
+		/* State Setup */
+		inturruptControl();
+		stateScheduleEvents();
+		screenResolve();
+	}
+
+}
+
+
+void stateScheduleEvents() {
+	switch (state) {
+	case UNLOCKED_EMPTY_ASLEEP:
+		eventRegister(accDeltaEvent, EVENT_ACCELEROMETER, EVENT_DELTA, 10, 0);
+		eventRegister(rotencDeltaEvent, EVENT_ROTARY_ENCODER, EVENT_DELTA, 1, 0);
+		break;
+
+	case UNLOCKED_ASLEEP_TO_AWAKE:
+		eventRegister(eventTimerCallback, EVENT_TIMER, EVENT_SINGLE, 5000, 0);
+		break;
+
+	case UNLOCKED_EMPTY_AWAKE:
+		eventRegister(nfcEventCallbackSlow, EVENT_NFC_READ, EVENT_DELTA, 1000, 0);
+		eventRegister(eventTimerCallback, EVENT_TIMER, EVENT_SINGLE, MINUTE, 0);
+		break;
+
+	case UNLOCKED_FULL_AWAKE_FUNC_A:
+		eventRegister(magBoxStatusEvent, EVENT_MAGNOMETER, EVENT_DELTA, 10, 0);
+		eventRegister(eventTimerCallback, EVENT_TIMER, EVENT_SINGLE, MINUTE, 0);
+		eventRegister(rotencDeltaEvent, EVENT_ROTARY_ENCODER, EVENT_DELTA, 1, 0);
+		break;
+
+	case UNLOCKED_FULL_AWAKE_FUNC_B:
+		eventRegister(magBoxStatusEvent, EVENT_MAGNOMETER, EVENT_DELTA, 10, 0);
+		eventRegister(eventTimerCallback, EVENT_TIMER, EVENT_SINGLE, MINUTE, 0);
+		eventRegister(rotencDeltaEvent, EVENT_ROTARY_ENCODER, EVENT_DELTA, 1, 0);
+		break;
+
+	case UNLOCKED_FULL_ASLEEP:
+		eventRegister(accDeltaEvent, EVENT_ACCELEROMETER, EVENT_DELTA, 10, 0);
+		eventRegister(magBoxStatusEvent, EVENT_ACCELEROMETER, EVENT_DELTA, 10, 0);
+		eventRegister(rotencDeltaEvent, EVENT_ROTARY_ENCODER, EVENT_DELTA, 1, 0);
+		break;
+
+	case UNLOCKED_TO_LOCKED_AWAKE:
+		eventRegister(eventTimerCallback, EVENT_TIMER, EVENT_SINGLE, 5000, 0);
+		break;
+
+	case LOCKED_FULL_AWAKE:
+		eventRegister(magBoxStatusEvent, EVENT_MAGNOMETER, EVENT_DELTA, 10, 0);
+		eventRegister(eventTimerCallback, EVENT_TIMER, EVENT_SINGLE, MINUTE, 0);
+		break;
+
+	case LOCKED_FULL_NOTIFICATION_FUNC_A:
+		eventRegister(magBoxStatusEvent, EVENT_MAGNOMETER, EVENT_DELTA, 10, 0);
+		eventRegister(eventTimerCallback, EVENT_TIMER, EVENT_SINGLE, MINUTE, 0);
+		eventRegister(rotencDeltaEvent, EVENT_ROTARY_ENCODER, EVENT_DELTA, 1, 0);
+		break;
+
+	case LOCKED_FULL_NOTIFICATION_FUNC_B:
+		eventRegister(magBoxStatusEvent, EVENT_ACCELEROMETER, EVENT_DELTA, 10, 0);
+		eventRegister(eventTimerCallback, EVENT_TIMER, EVENT_SINGLE, MINUTE, 0);
+		eventRegister(rotencDeltaEvent, EVENT_ROTARY_ENCODER, EVENT_DELTA, 1, 0);
+		break;
+
+	case LOCKED_FULL_ASLEEP:
+		eventRegister(accDeltaEvent, EVENT_ACCELEROMETER, EVENT_DELTA, 10, 0);
+		eventRegister(magBoxStatusEvent, EVENT_ACCELEROMETER, EVENT_DELTA, 10, 0);
+		eventRegister(rotencDeltaEvent, EVENT_ROTARY_ENCODER, EVENT_DELTA, 1, 0);
+		break;
+
+	case LOCKED_MONITOR_AWAKE:
+		eventRegister(magBoxStatusEvent, EVENT_ACCELEROMETER, EVENT_DELTA, 10, 0);
+		eventRegister(eventTimerCallback, EVENT_TIMER, EVENT_SINGLE, MINUTE, 0);
+		eventRegister(audioEventCallback, EVENT_AUDIO, EVENT_DELTA, 1, 0);
+		break;
+
+	case LOCKED_MONITOR_ASLEEP:
+		eventRegister(accDeltaEvent, EVENT_ACCELEROMETER, EVENT_DELTA, 10, 0);
+		eventRegister(magBoxStatusEvent, EVENT_ACCELEROMETER, EVENT_DELTA, 10, 0);
+		eventRegister(eventTimerCallback, EVENT_TIMER, EVENT_SINGLE, MINUTE, 0);
+		eventRegister(rotencDeltaEvent, EVENT_ROTARY_ENCODER, EVENT_DELTA, 1, 0);
+		eventRegister(audioEventCallback, EVENT_AUDIO, EVENT_DELTA, 1, 0);
+		break;
+
+	default:
+		break;
+	}
+}
+
+#ifdef DEBUG_STATE_CONTROLLER
+const char* SFlagToStr(SFlag flag) {
+	switch (flag) {
+	case SFLAG_NULL: return "SFLAG_NULL";
+	case SFLAG_ACC_BOX_MOVED: return "SFLAG_ACC_BOX_MOVED";
+	case SFLAG_ADC_INTERRUPT: return "SFLAG_ADC_INTERRUPT";
+	case SFLAG_ADC_MATCH: return "SFLAG_ADC_MATCH";
+	case SFLAG_ROTENC_INTERRUPT: return "SFLAG_ROTENC_INTERRUPT";
+	case SFLAG_ROTENC_ROTATED: return "SFLAG_ROTENC_ROTATED";
+	case SFLAG_TIMER_COMPLETE: return "SFLAG_TIMER_COMPLETE";
+	case SFLAG_NFC_PHONE_PRESENT: return "SFLAG_NFC_PHONE_PRESENT";
+	case SFLAG_NFC_PHONE_NOT_PRESENT: return "SFLAG_NFC_PHONE_NOT_PRESENT";
+	case SFLAG_BOX_CLOSED: return "SFLAG_BOX_CLOSED";
+	case SFLAG_BOX_OPEN: return "SFLAG_BOX_OPEN";
+	case SFLAG_AUDIO_VOL_HIGH: return "SFLAG_AUDIO_VOL_HIGH";
+	case SFLAG_AUDIO_MATCH: return "SFLAG_AUDIO_MATCH";
+	case SFLAG_AUDIO_NO_MATCH: return "SFLAG_AUDIO_NO_MATCH";
+	default: return "UNKNOWN_SFLAG";
+	}
+}
+#endif
+
